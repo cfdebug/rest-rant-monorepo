8345ac34c9e999f8a15da9eb58cabd4c598ad739
cfdebug <rfadventure22@gmail.com>
2023-01-10 22:42:57 -0600

    Added JWT Authentication

--- backend/controllers/authentication.js
+++ backend/controllers/authentication.js
@@ -1,6 +1,7 @@
 const router = require('express').Router()
 const db = require("../models")
 const bcrypt = require('bcrypt')
+const jwt = require('json-web-token')
 
 const { User } = db
 
@@ -17,7 +18,35 @@ router.post('/', async (req, res) => {
             message: `Could not find a user with the provided username and password` 
         })
     } else {
-        res.json({ user: user })
+        const result = await jwt.encode(process.env.JWT_SECRET, {id: user.userId})
+        res.json({ user: user, token: result.value })
+    }
+})
+
+router.get('/profile', async (req, res) => {
+    try {
+        // Split Header
+        const [authenticationMethod, token] = req.headers.authorization.split(' ')
+
+        // Handle Bearer Method
+        if (authenticationMethod == 'Bearer') {
+
+            // Decode JWT
+            const result = await jwt.decode(process.env.JWT_SECRET, token)
+
+            // Obtain ID
+            const { id } = result.value
+            
+            // Find User with ID
+            let user = await User.findOne({
+                where: {
+                    userId: id 
+                }
+            })
+            res.json(user)
+        }
+    } catch {
+        res.json(null)
     }
 })
 
